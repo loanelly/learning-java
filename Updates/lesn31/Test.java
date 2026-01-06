29de4a87ddacfc1b79e3422d05334e77178f56d6
lonly <egardshan@gmail.com>
2026-01-06 21:40:35 +0300

    Урок 31 - классы обертки приметивных типов данных

---
+++ Updates/lesn31/Test.java
@@ -0,0 +1,14 @@
+package lesn31;
+
+public class Test {
+    // урок 31 - Классы - обертки приметивных типов данных
+    public static void main(String[] args){
+        int x = 1;
+        // классы обертки -
+        // Double, Float, Integer, Short, Byte, Charecter, Booean
+        //Integer integer = new Integer(1);
+        Integer x2 = 123;//запоковка приметива в обертку
+        int y = x2;// Распоковали обратно
+        // Классы обертки нужны чтобы вызывать методы у приметивных типов данных
+    }
+}
